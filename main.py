810059d4f8cdbf9979129556b54b29eb951a8d6f
Margo <gosia0071@gmail.com>
2023-02-01 19:35:00 +0100

    EAN enter manually field

--- main.py
+++ main.py
@@ -30,38 +30,43 @@ class InformationProductPage(MDScreen):
     passProductName = StringProperty('')
 
 
-
-
-
 class SearchIngredientsPage(MDScreen):
     getIngr = StringProperty('')
 
     def search(self):
-        self.getIngr = self.print_ingr_text_input.text
-        splitted = self.getIngr.split(',')
-        print(splitted[0])
+        self.getIngr = self.print_ingr_text_input.text.upper()
+        self.splitted = self.getIngr.split(',')
+        print(self.splitted[0])
         secretKey = "RiOrjHCoyneB4MNVV3P60ieFrlLaBP6l"
         accessKey = "bdf0a6c6289df4c7"
 
-        for item in splitted:
-            unified_item = item.replace(" ", "+")
-            print(unified_item)
-            path = f"/ingredient/search/{unified_item}?accessKeyId={accessKey}"
+        for item in self.splitted:
+            self.unified_item = item.strip().replace("/", " ").replace(" ", "+")
+            #print(self.unified_item)
+            path = f"/ingredient/search/{self.unified_item}?accessKeyId={accessKey}"
             hmac_val = hmac.new(bytes(secretKey, "UTF-8"), path.encode("UTF-8"), hashlib.sha256).hexdigest()
-            url = f"http://api.incibeauty.com/ingredient/search/{unified_item}?accessKeyId={accessKey}&hmac={hmac_val}"
+            url = f"http://api.incibeauty.com/ingredient/search/{self.unified_item}?accessKeyId={accessKey}&hmac={hmac_val}"
             response = requests.get(url)
             parsed = response.json()
-            for ingr in parsed['docs']:
+            print(parsed)
+            parsed_ingr = [x for x in parsed['docs'] if x['inci_name'] == item.strip()]
+
+            #tutaj dac sprawdzanie czy pole jest puste czy nie
+
+            for ingr in parsed_ingr:
                 ingr_path = f"/ingredient/{ingr['identifier']}?locale=pl_PL&accessKeyId={accessKey}"
                 ingr_hmac = hmac.new(bytes(secretKey, "UTF-8"), ingr_path.encode("UTF-8"), hashlib.sha256).hexdigest()
                 ingr_url = f"http://api.incibeauty.com/ingredient/{ingr['identifier']}?locale=pl_PL&accessKeyId={accessKey}&hmac={ingr_hmac}"
                 ingr_response = requests.get(ingr_url)
-                print(ingr_response.json())
+                getInciName = ingr_response.json().get("inci_name")
+                self.InciDescr = ingr_response.json().get("description")
+                self.manager.get_screen('ingr_inf_page').ids.ingr_show.text += f'{getInciName}:  {self.InciDescr}, \n'
 
 
 
 class ScanBarcodePage(MDScreen):
     scannedEAN = ''
+    EntermanEan = StringProperty('')
 
     def get_product_by_ean(self, symbols):
         if symbols is not None and len(symbols) > 0:
@@ -74,6 +79,15 @@ class ScanBarcodePage(MDScreen):
             #print(self.scannedEAN)
 
     def get_product(self):
+
+        #tutaj tez sprawdaanie czy pole jest puste
+        self.EntermanEan = self.enter_ean_man.text
+
+        if self.EntermanEan:
+            self.scannedEAN = self.EntermanEan
+        #else:
+
+
         secretKey = "RiOrjHCoyneB4MNVV3P60ieFrlLaBP6l"
         path = f"/product/composition/{self.scannedEAN}?locale=pl_PL&accessKeyId=bdf0a6c6289df4c7"
         hmac_val = hmac.new(bytes(secretKey, "UTF-8"), path.encode("UTF-8"), hashlib.sha256).hexdigest()
